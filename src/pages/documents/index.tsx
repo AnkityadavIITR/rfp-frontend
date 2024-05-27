09164726446598bf9b4ce1abec58e48b6553c0db
AnkityadavIITR <ankit_y@ar.iitr.ac.in>
2024-05-27 07:48:31 +0000

    dev changes

--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -25,7 +25,7 @@ interface PdfViewerProps {
 export default function Conversation() {
   const router = useRouter();
   const { isMobile } = useIsMobile();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [isFetching, setIsFetching] = useState(false);
 
   const queries = useQuestionStore((state) => state.queries);
@@ -33,10 +33,10 @@ export default function Conversation() {
   const addResponse = useQuestionStore((state) => state.addResponse)
   const fileUrls = useQuestionStore((state) => state.fileUrls)
   const addFileUrls = useQuestionStore((state) => state.addFileUrl);
-  const activeQuery=useQuestionStore((state)=>state.activeQuery);
-  const setActiveQuery=useQuestionStore((state)=>state.setActiveQuery);
-  const addApiResponse=useQuestionStore((state)=>state.addApiResponse);
-  const apiResponse=useQuestionStore((state)=>state.apiResponse);
+  const activeQuery = useQuestionStore((state) => state.activeQuery);
+  const setActiveQuery = useQuestionStore((state) => state.setActiveQuery);
+  const addApiResponse = useQuestionStore((state) => state.addApiResponse);
+  const apiResponse = useQuestionStore((state) => state.apiResponse);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,12 +44,12 @@ export default function Conversation() {
       try {
         await Promise.all(
           queries.map(async (question, index) => {
-            const res = await backendClient.fetchQuery("/processquery/", question);            
+            const res = await backendClient.fetchQuery("/processquery/", question);
             const responseData = await res.json();
-            console.log("res",responseData);
-            
-            console.log("message",responseData.message);
-            const response=formatMarkdown(responseData.message)
+            console.log("res", responseData);
+
+            console.log("message", responseData.message);
+            const response = formatMarkdown(responseData.message)
             addResponse(response);
 
             // addApiResponse({
@@ -65,12 +65,13 @@ export default function Conversation() {
               const existingFile = fileUrls.find(file => file.filename === pdfName);
               if (!existingFile) {
                 addFileUrls({
-                  id:uuid(),
+                  id: uuid(),
                   filename: pdfName,
                   url: responseData.fileUrls[index]
                 });
               }
             });
+            setLoading(false);
           }))
       } catch (e) {
         console.error(e);
@@ -83,17 +84,17 @@ export default function Conversation() {
       fetchData();
     }
   }, []);
-  console.log("apiresponse",apiResponse);
-  
+  console.log("apiresponse", apiResponse);
+
 
   const formatMarkdown = (message: string): string => {
     const lines: string[] = message.split('\n');
     let formattedMessage: string = '';
-  
+
     let currentHeadingLevel: number = 2;
     let currentNumber: number | null = null;
     let currentSubheadingLetter: string = 'a';
-  
+
     lines.forEach((line: string, index: number) => {
       const numberMatch = line.match(/^\d+\./);
       if (numberMatch && numberMatch[0]) {
@@ -118,32 +119,9 @@ export default function Conversation() {
         currentHeadingLevel = 2;
       }
     });
-  
+
     return formattedMessage;
   };
-
-  const pdfData = [
-    {
-      ticker: "sacadc",
-      fullName: "ankit",
-      id: "kcsdc",
-      url: "https://d687lz8k56fia.cloudfront.net/sec-edgar-filings/0000320193/10-K/0000320193-23-000106/primary-document.pdf",
-      year: "2012",
-      quarter: "2",
-      color: DocumentColorEnum.purple,
-    },
-    {
-      ticker: "dscnjsbdv",
-      fullName: "ankit-2",
-      id: "sdcuydvdv",
-      url: "https://d687lz8k56fia.cloudfront.net/sec-edgar-filings/0000200406/10-Q/0000200406-23-000082/filing-details.pdf",
-      year: "2012",
-      quarter: "2",
-      color: DocumentColorEnum.purple,
-    },
-  ];
-
-
 
   if (isMobile) {
     return (
@@ -170,64 +148,64 @@ export default function Conversation() {
 
   return (
     <>
-          <PdfFocusProvider>
-            <div className="flex h-[100vh] w-full items-center">
-              <div className="flex h-[100vh] w-[44vw] flex-col items-center border-r-2 bg-white">
-                <div className="flex h-[44px] w-full items-center justify-between border-b-2 ">
-                  <div className="flex w-full items-center justify-between">
-                    <button
-                      onClick={() => {
-                        clearData();
-                        router
-                          .push("/")
-                          .catch(() => console.error("error navigating home"));
-                      }}
-                      className="ml-4 flex items-center justify-center rounded px-2 font-light text-[#9EA2B0] hover:text-gray-900"
-                    >
-                      <BiArrowBack className="mr-1" /> Back to Document Selection
-                    </button>
-                  </div>
-                </div>
-                <div className="flex h-full w-[44vw] flex-grow flex-col overflow-scroll ">
-                  <div className="mx-auto  w-[100%] flex flex-col text-left">
-                    {
-                      queries.map((query, i) => {
-                        return (
-                          <Accordion
-                            type="single"
-                            collapsible
-                            className="flex flex-col  "
-                            key={i}
-                            defaultValue="item-0"
-                          >
-                            <AccordionItem
-                               value={`item-${i}`}
-                              className=" bg-gray-200 text-left"
-                            >
-                              <AccordionTrigger className="text-left p-[10px]">{query}</AccordionTrigger>
-                              <AccordionContent className="bg-white p-[10px] mb-0 text-gray-700">
-                                {responses[i] ? <ReactMarkdown className="leading-1">{responses[i]}</ReactMarkdown> : <p className="mx-auto">...processing</p>}
-                              </AccordionContent>
-                            </AccordionItem>
-                            <hr className="w-[2px] bg-gray-700" />
-
-                          </Accordion>
-
-                        )
-                      })
-                    }
-                  </div>
-
-                </div>
-              </div>
-              <div className="h-[100vh] w-full">
-                {
-                  loading ? <div className="w-full h-full flex justify-center items-center "><h1>loading...</h1></div>:<DisplayMultiplePdfs fileUrls={fileUrls} />
-                }
-                
+      <PdfFocusProvider>
+        <div className="flex h-[100vh] w-full items-center">
+          <div className="flex h-[100vh] w-[44vw] flex-col items-center border-r-2 bg-white">
+            <div className="flex h-[44px] w-full items-center justify-between border-b-2 ">
+              <div className="flex w-full items-center justify-between">
+                <button
+                  onClick={() => {
+                    clearData();
+                    router
+                      .push("/")
+                      .catch(() => console.error("error navigating home"));
+                  }}
+                  className="ml-4 flex items-center justify-center rounded px-2 font-light text-[#9EA2B0] hover:text-gray-900"
+                >
+                  <BiArrowBack className="mr-1" /> Back to Document Selection
+                </button>
               </div>
             </div>
-          </PdfFocusProvider>
+            <div className="flex h-full w-[44vw] flex-grow flex-col overflow-scroll ">
+              <div className="mx-auto  w-[100%] flex flex-col text-left">
+                {
+                  queries.map((query, i) => {
+                    return (
+                      <Accordion
+                        type="single"
+                        collapsible
+                        className="flex flex-col  "
+                        key={i}
+                        defaultValue="item-0"
+                      >
+                        <AccordionItem
+                          value={`item-${i}`}
+                          className=" bg-gray-200 text-left"
+                        >
+                          <AccordionTrigger className="text-left p-[10px]">{query}</AccordionTrigger>
+                          <AccordionContent className="bg-white p-[10px] mb-0 text-gray-700">
+                            {responses[i] ? <ReactMarkdown className="leading-1">{responses[i]}</ReactMarkdown> : <><div className="loader h-4 w-4 rounded-full border-2 border-gray-200 ease-linear"></div><p className="mr-1">processing</p></>}
+                          </AccordionContent>
+                        </AccordionItem>
+                        <hr className="w-[2px] bg-gray-700" />
+
+                      </Accordion>
+
+                    )
+                  })
+                }
+              </div>
+
+            </div>
+          </div>
+          <div className="h-[100vh] w-full">
+            {
+              loading ? <div className="w-full h-full flex justify-center items-center "><h1>loading...</h1></div> : <DisplayMultiplePdfs fileUrls={fileUrls} />
+            }
+
+          </div>
+        </div>
+      </PdfFocusProvider>
 
 
     </>
