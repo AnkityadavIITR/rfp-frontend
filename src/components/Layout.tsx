ffc14b22d7da7f3344aaf23a7d317d199b760478
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-05-28 20:01:51 +0530

    feat: added clerk sdk, setup auth navbar in app

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-import type { PropsWithChildren } from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { type PropsWithChildren } from "react";
+
 const Layout = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+  return (
+    <div className=" flex h-screen w-screen flex-col">
+      <div className="flex h-10 items-center justify-end px-2">
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="rounded bg-blue-500 px-4 py-1 font-bold text-white hover:bg-blue-700">
+              Sign in
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+      {children}
+    </div>
+  );
 };
 export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import Layout from "~/components/Layout";
 import "~/styles/globals.css";
 import ReactGA from "react-ga4";
-import { GOOGLE_ANALYTICS_ID} from "~/constants";
+import Layout from "~/components/Layout";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { GOOGLE_ANALYTICS_ID } from "~/constants";
+import { type AppType } from "next/dist/shared/lib/utils";
 
 ReactGA.initialize(GOOGLE_ANALYTICS_ID);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-    </>
+    <ClerkProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ClerkProvider>
   );
 };
 
