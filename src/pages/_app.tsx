2673ff9486a414af5346c40a3847f62702785a8c
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-05-29 14:03:47 +0530

    feat: clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,24 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import Layout from "~/components/Layout";
 import "~/styles/globals.css";
 import ReactGA from "react-ga4";
-import { GOOGLE_ANALYTICS_ID} from "~/constants";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { GOOGLE_ANALYTICS_ID } from "~/constants";
+import Layout from "~/components/Layout";
+import { type AppType } from "next/dist/shared/lib/utils";
 // import { PdfFocusProvider } from "~/context/pdf";
+
 ReactGA.initialize(GOOGLE_ANALYTICS_ID);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <>
-    {/* <PdfFocusProvider> */}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-
-    {/* </PdfFocusProvider> */}
-
-    </>
+    <ClerkProvider>
+      {/* <PdfFocusProvider> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </PdfFocusProvider> */}
+    </ClerkProvider>
   );
 };
 
