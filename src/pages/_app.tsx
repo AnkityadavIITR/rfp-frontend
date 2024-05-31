8a1259a5633af83301e97509496bc571d1ee45f9
AnkitIITR <ankit_y@ar.iitr.ac.in>
2024-05-31 15:40:03 +0530

    solve zusatnd hydration error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,20 @@ import { GOOGLE_ANALYTICS_ID } from "~/constants";
 import Layout from "~/components/Layout";
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Toaster } from "~/components/ui/toaster";
+import HydrationZustand from "~/components/HydrationZustand";
 
 ReactGA.initialize(GOOGLE_ANALYTICS_ID);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
+      <HydrationZustand>
       <Layout>
         <Component {...pageProps} />
         <Toaster/>
       </Layout>
+
+      </HydrationZustand>
     </ClerkProvider>
   );
 };
